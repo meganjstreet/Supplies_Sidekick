116713129e4e146566230c5c307aa079be510b15
Megan Street <meganjstreet@gmail.com>
2024-05-28 16:45:38 +0200

    Creating dashboard with supplies for sale and pending offers and alerts if you don't have any yet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
-
   def home
   end
 
   def dashboard
-    
+    @user = current_user
+    @supplies = @user.supplies
+    @offers = @user.offers
   end
 end
