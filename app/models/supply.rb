b4f723952a9622aaa8847c1306704f62c32ebbe3
Megan Street <meganjstreet@gmail.com>
2024-05-29 12:50:37 +0200

    Removing map stuff

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -2,6 +2,5 @@ class Supply < ApplicationRecord
   validates :name, :description, :address, :price, :quantity, presence: true
   belongs_to :user
   has_many :offers
-  after_validation :geocode, if: :will_save_change_to_address?
   has_one_attached :photo
 end
