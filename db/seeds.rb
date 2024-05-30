ddef16e09d4b3d558e2d5e6353f31f9142b4d67b
Nolusindiso Ncamiso <nncamiso30108@gmail.com>
2024-05-30 12:50:58 +0200

    adding offers to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,26 +14,32 @@ User.destroy_all
 
 puts "Making some supplies seeds"
 
-user = User.new(first_name: "Meg", last_name: "Street", address: "89 Geneva Drive, Camps Bay, Cape Town", email: "meganjstreet@gmail.com", password:"secret", avatar_url:"https://thumbs.dreamstime.com/b/construction-blueprint-5200849.jpg")
-user2 = User.new(first_name: "Nolu", last_name: "Ncamiso", address: "Gugulethu ny119 street no6", email: "nncamiso30108@gmail.com", password:"nolu-2024N",avatar_url:"https://www.dreamstime.com/stock-photo-tired-construction-worker-wiping-forehead-site-image85305078.jpg")
-user3 = User.new(first_name: "Luvo", last_name: "Sapa", address: "Kraaifotain wallacedene,france street 27360", email: "sapaluvo@gmail.com", password:"luvos-2024", avatar_url:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fphotos%2Fconstruction-engineer.html&psig=AOvVaw2DDwanjdZejcQhlPYgdHCj&ust=1717063824554000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMio_7XPsoYDFQAAAAAdAAAAABAJ.jpg")
-user4 = User.new(first_name: "Tools", last_name: "Mavalentiya", address: " Phillip village,Cape Town", email: "Athule@gmail.com", password:"tools-2024", avatar_url:"https://thumbs.dreamstime.com/b/construction-blueprint-5200849.jpg")
-user.save!
-user2.save!
-user3.save!
-user4.save!
+User.create(first_name: "Meg", last_name: "Street", address: "89 Geneva Drive, Camps Bay, Cape Town", email: "meganjstreet@gmail.com", password:"secret", avatar_url:"https://thumbs.dreamstime.com/b/construction-blueprint-5200849.jpg")
+User.create(first_name: "Nolu", last_name: "Ncamiso", address: "Gugulethu ny119 street no6", email: "nncamiso30108@gmail.com", password:"secret",avatar_url:"https://www.dreamstime.com/stock-photo-tired-construction-worker-wiping-forehead-site-image85305078.jpg")
+User.create(first_name: "Luvo", last_name: "Sapa", address: "Kraaifotain wallacedene,france street 27360", email: "sapaluvo@gmail.com", password:"secret", avatar_url:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fphotos%2Fconstruction-engineer.html&psig=AOvVaw2DDwanjdZejcQhlPYgdHCj&ust=1717063824554000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMio_7XPsoYDFQAAAAAdAAAAABAJ.jpg")
+User.create(first_name: "Tools", last_name: "Mavalentiya", address: " Phillip village,Cape Town", email: "Athule@gmail.com", password:"secret", avatar_url:"https://thumbs.dreamstime.com/b/construction-blueprint-5200849.jpg")
 
-Supply.create(name: "Wood Planks", description: "1000 wooden planks", address: "123 Avenue", img_url:"https://housing.com/news/wp-content/uploads/2023/04/What-is-timber-wood-and-which-are-the-best-types-f.jpg", price:100, quantity:100, user: user)
-Supply.create(name: "Paint", description: "200 buckets of paint", address: "666 Street", img_url:"https://www.builders.co.za/media/sys-master%2Froot%2Fh39%2Fh3f%2F12683974639646%2F99-423601-Straight+on_large", price:69, quantity:2, user: user2 )
-Supply.create(name: "Nails", description: "Big pot of nails", address: "123 Avenue", img_url:"https://housing.com/news/wp-content/uploads/2023/04/What-is-timber-wood-and-which-are-the-best-types-f.jpg", price:888, quantity:4, user: user)
-Supply.create(name: "Sand paper", description: "5 sheets of sandpaper", address: "999 Road", img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Schleifpapier_verschiedene_Sorten.jpg/800px-Schleifpapier_verschiedene_Sorten.jpg", price:230, quantity:5, user: user2)
-Supply.create(name: "Screwdriver set", description: "screwdriver set in good condition", address: "345 Burb", img_url: "https://www.bobvila.com/wp-content/uploads/2017/04/Types_Of_Screwdrivers.jpg", price:30, quantity:3967, user: user)
 
-Supply.create(name: "Scaffold hoists", description: "Scaffold hoists set in good condition", address: "phillip junction mall", img_url: "https://www.hoistandaccess.com/assets/uploads/images/equipment/scaffold-hoist-3.jpg", price:750, quantity:4380, user: user)
-Supply.create(name: "Plastic Rebar spacers", description: "Plastic Rebar spacers set in good condition", address: "50 long street capetown", img_url: "rebar.jpg", price:280, quantity:6990, user: user)
-Supply.create(name: "Galvanised Hoop Iron", description: "Galvanised Hoop Iron set in good condition", address: "Corner of Bill Bezuidenhout Avenue", img_url:"hoops.jpg", price:30, quantity:2540, user: user)
-Supply.create(name: "PPR Pipes, Fittings and Coupling", description: "PPR Pipes, Fittings and Coupling set in good condition", address: "Jakes gerwel Dr,Athlone,capetown,7764", img_url: "pprpipes.jpg", price:189.99, quantity:2890, user: user)
+
+Supply.create(name: "Wood Planks", description: "1000 wooden planks", address: "123 Avenue", img_url:"https://housing.com/news/wp-content/uploads/2023/04/What-is-timber-wood-and-which-are-the-best-types-f.jpg", price:100, quantity:100, user: User.find_by(first_name: "Meg"))
+Supply.create(name: "Paint", description: "200 buckets of paint", address: "666 Street", img_url:"https://www.builders.co.za/media/sys-master%2Froot%2Fh39%2Fh3f%2F12683974639646%2F99-423601-Straight+on_large", price:450, quantity:2, user: User.find_by(first_name: "Nolu") )
+Supply.create(name: "Nails", description: "Big pot of nails", address: "123 Avenue", img_url:"https://housing.com/news/wp-content/uploads/2023/04/What-is-timber-wood-and-which-are-the-best-types-f.jpg", price:888, quantity:4, user: User.find_by(first_name: "Meg"))
+Supply.create(name: "Sand paper", description: "5 sheets of sandpaper", address: "999 Road", img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Schleifpapier_verschiedene_Sorten.jpg/800px-Schleifpapier_verschiedene_Sorten.jpg", price:230, quantity:5, user: User.find_by(first_name: "Nolu"))
+Supply.create(name: "Screwdriver set", description: "screwdriver set in good condition", address: "345 Burb", img_url: "https://www.bobvila.com/wp-content/uploads/2017/04/Types_Of_Screwdrivers.jpg", price:30, quantity:3967, user: User.find_by(first_name: "Meg"))
+
+Supply.create(name: "Scaffold hoists", description: "Scaffold hoists set in good condition", address: "phillip junction mall", img_url: "https://www.hoistandaccess.com/assets/uploads/images/equipment/scaffold-hoist-3.jpg", price:750, quantity:4380, user: User.find_by(first_name: "Meg"))
+Supply.create(name: "Plastic Rebar spacers", description: "Plastic Rebar spacers set in good condition", address: "50 long street capetown", img_url: "rebar.jpg", price:280, quantity:6990, user: User.find_by(first_name: "Meg"))
+Supply.create(name: "Galvanised Hoop Iron", description: "Galvanised Hoop Iron set in good condition", address: "Corner of Bill Bezuidenhout Avenue", img_url:"hoops.jpg", price:30, quantity:2540, user: User.find_by(first_name: "Meg"))
+Supply.create(name: "PPR Pipes, Fittings and Coupling", description: "PPR Pipes, Fittings and Coupling set in good condition", address: "Jakes gerwel Dr,Athlone,capetown,7764", img_url: "pprpipes.jpg", price:189.99, quantity:2890, user: User.find_by(first_name: "Meg"))
+
+Offer.create(amount: 750, details:"Can I have Wood Planks", user: User.find_by(first_name: "Meg"), supply: Supply.find_by(name: "Wood Planks"))
+Offer.create(amount: 380, details:"Can I have White Paint", user: User.find_by(first_name: "Tools"), supply: Supply.find_by(name: "Paint"))
+Offer.create(amount: 170, details:"Got Galvanised Hoop Iron, In a good condition", user: User.find_by(first_name: "Meg"), supply: Supply.find_by(name:"Galvanised Hoop Iron"))
+Offer.create(amount: 700, details:"1 year old Scaffold hoists",user: User.find_by(first_name: "Meg"), supply: Supply.find_by(name:" Scaffold hoists"))
+Offer.create(amount: 830, details:"Looking for PPR Pipes ,Fitting and Coupling for my New Bathroom",user: User.find_by(first_name:"Luvo"), supply: Supply.find_by(name: "PPR Pipes, Fittings and Coupling"))
 
 puts "Supplies seeded!"
 
 # img_url: "homepage-banner.jpg
+# puts Supply.where(name: "Paint")
+# puts Supply.find(id: 1)
